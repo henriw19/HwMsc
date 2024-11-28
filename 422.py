f8b2d59781555994a769fa2b23c6d2a7270a90f2
henriw81 <henri.wegener12@gmail.com>
2024-11-28 13:02:16 +0100

    vanilla floquet comparison

--- 422.py
+++ 422.py
@@ -39,7 +39,8 @@ def periodic422(t):
         H 4
         CZ 4 0 4 1 4 2 4 3
         H 4
-        M 4         
+        M 4      
+        R 4   
     ''')
 
     for i in range(t):
@@ -59,17 +60,23 @@ def periodic422(t):
             circuit.append("DETECTOR", [stim.target_rec(-1), stim.target_rec(-3)])
 
         # ZZZZ
-        circuit += Circuit('''      
-            H 4
-            CZ 4 0 4 1 4 2 4 3
-            H 4
-            M 4
-            R 4
-        ''')
+        # circuit += Circuit('''      
+        #     H 4
+        #     CZ 4 0 4 1 4 2 4 3
+        #     H 4
+        #     M 4
+        #     R 4
+        # ''')
+        circuit += Circuit('''
+                            CX 0 4 1 4 2 4 3 4
+                            M 4
+                            R 4
+                            ''')
         
         # Detectors
-        circuit.append(f"DETECTOR", [stim.target_rec(-1), stim.target_rec(-3)])
-        
+        circuit.append("DETECTOR", [stim.target_rec(-1), stim.target_rec(-3)])
+    
+
 
     # measure logical Z
     circuit += Circuit('''   
@@ -77,9 +84,10 @@ def periodic422(t):
             M 6
             OBSERVABLE_INCLUDE(0) rec[-1] 
         ''')
+
     return circuit
 
-def floquet422(L,p=0):
+def floquet422(L):
     def measurex(target_qubits, ancilla):
         return Circuit(f'''
                             H {ancilla}
@@ -117,9 +125,6 @@ def floquet422(L,p=0):
             
             # outer hexagon measurement #1
             circuit += m[t%2]([(t * 2 + 3) % 12, (t * 2 + 5) % 12], 15)
-
-            # if t == 3:
-            #     circuit += Circuit(f'DEPOLARIZE1({p}) 8 3 2 5')
             
             # radial measurement
             circuit += m[t%2]([(t * 2 + 6) % 12, (t * 2 + 7) % 12], 16)
@@ -138,18 +143,19 @@ def floquet422(L,p=0):
         circuit.append("DETECTOR", [stim.target_rec(-54), stim.target_rec(-53), stim.target_rec(-49), stim.target_rec(-48), stim.target_rec(-26), stim.target_rec(-25), stim.target_rec(-24), stim.target_rec(-23)])
         
     # measure logical Z
-    circuit += Circuit('''   
-                   OBSERVABLE_INCLUDE(0) rec[-54] rec[-52] rec[-49] rec[-40] rec[-38] rec[-35] rec[-26] rec[-24] rec[-21] rec[-4] rec[-6] rec[-10] rec[-12] 
-        ''')
     
-    # # measure logical X
-    # circuit += Circuit('''   
-    #                OBSERVABLE_INCLUDE(0) rec[-31] rec[-30] rec[-29] rec[-15] rec[-16] rec[-17] rec[-3] rec[-2] rec[-1] 
-    #     ''')
-    
+        if l == L-1:
+            circuit += Circuit('''   
+                    OBSERVABLE_INCLUDE(0) rec[-52] rec[-49] rec[-38] rec[-35] rec[-24] rec[-21] rec[-4] rec[-6] rec[-10] rec[-12] 
+            ''')
+        else:
+            circuit += Circuit('''   
+                    OBSERVABLE_INCLUDE(0) rec[-52] rec[-49] rec[-38] rec[-35] rec[-24] rec[-21] 
+            ''')
+       
     return circuit
 
-def addnoise(circuit, faulty_gates, p_depol = 0):
+def addnoise(circuit, faulty_gates, single_qubit_noise = 0, multi_qubit_noise = 0):
     noisy_circuit = stim.Circuit()
     for operation in circuit:
         operation = str(operation).split(' ')
@@ -162,27 +168,22 @@ def addnoise(circuit, faulty_gates, p_depol = 0):
 
             # Apply depolarizing noise after each faulty gate
             if len(target_qubits) > 1:
-                pass
-                # for i,qubit in enumerate(target_qubits[::2]):
-                #     print(i,qubit)
-                #     noisy_circuit.append_operation("DEPOLARIZE2", [target_qubits[i * 2], target_qubits[(i * 2 + 1)]], p_depol)
+                for i,qubit in enumerate(target_qubits[::2]):
+                    noisy_circuit.append_operation("DEPOLARIZE2", [target_qubits[i * 2], target_qubits[(i * 2 + 1)]], multi_qubit_noise)
             else:
                 for target in target_qubits:
-                    noisy_circuit.append_operation("DEPOLARIZE1", [target], p_depol)
-            
-
+                    noisy_circuit.append_operation("DEPOLARIZE1", [target], single_qubit_noise)
         else:
             noisy_circuit += Circuit(f'''{gate} {" ".join(target_qubits)}''')
     return noisy_circuit
 
-def log_err_counter(circuits, ps, num_shots):
+def log_err_counter(circuits, single_qubit_noise, multi_qubit_noise, num_shots):
     for t,circ in enumerate(circuits):
         xs = []
         ys = []
         
-        for noise in ps:
-            noisy_circuit = addnoise(circ, {"H","M","CZ","CX","R"}, noise)
-
+        for i,noise in enumerate(single_qubit_noise):
+            noisy_circuit = addnoise(circ, {"H","M","CZ","CX","R"}, single_qubit_noise[i], multi_qubit_noise[i])
             sampler = noisy_circuit.compile_detector_sampler()
             detection_events, observable_flips = sampler.sample(num_shots, separate_observables=True)
             dem = noisy_circuit.detector_error_model(decompose_errors=True)
@@ -196,33 +197,25 @@ def log_err_counter(circuits, ps, num_shots):
                     num_errors += 1
             xs.append(noise)
             ys.append(num_errors / num_shots)
-        plt.plot(xs, ys, label="L=" + str(t))
+        
+        plt.plot(xs, ys, label="circuit" + str(t))
     plt.loglog()
     plt.xlabel("physical error rate")
     plt.ylabel("logical error rate per shot")
     plt.legend()
-    # print(repr(noisy_circuit))
     plt.show()
-    # plt.savefig("logerr.png")
 
 def main():
-      
-    circ = floquet422(1)
-    
-    sampler = circ.compile_detector_sampler()
-    # data = sampler.sample(shots=100)
-    dem = circ.detector_error_model(decompose_errors=True)
-    # print(dem)
-    # log_err_counter([circ], np.logspace(-4,-.3,7), 10000)
-    # print(repr(circ))
-    
-    # Find constant and changing indices
-    # constant_indices = np.where(np.all(data == data[0], axis=0))[0]  # Indices where all values match
-    # changing_indices = np.where(~np.all(data == data[0], axis=0))[0]  # Indices where values differ
+    circ0 = periodic422(5)
+    circ1 = periodic422(10)
+    circ2 = periodic422(15)
 
-    # # Print results
-    # print("Constant Indices:", constant_indices)
-    # print("Changing Indices:", changing_indices)
+    circ3 = floquet422(5)
+    circ4 = floquet422(10)
+    circ5 = floquet422(15)
+    
+   
+    log_err_counter([circ0,circ1,circ2,circ3,circ4,circ5], np.logspace(-4,-.7,5),np.logspace(-4,-.7,5) , 100000)
 
 
 if __name__ == "__main__":
