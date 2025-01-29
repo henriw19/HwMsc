4eecc8d7ab8961247b90727029aa2b243d3a2697
OzYossarian <38085891+OzYossarian@users.noreply.github.com>
2025-01-29 14:46:53 +0100

    Creating detector schedule (won't work without changes in Kandel codebase, which is still WIP).

--- kandel_floquetified_colour_code.py
+++ kandel_floquetified_colour_code.py
@@ -1,18 +1,27 @@
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 
 import numpy as np
 from main.building_blocks.Check import Check
 from main.building_blocks.Qubit import Qubit
+from main.building_blocks.detectors.Detector import Detector
 from main.building_blocks.pauli import Pauli
 from main.building_blocks.pauli.PauliLetter import PauliLetter
 from main.codes.Code import Code
 from main.printing.Printer2D import Printer2D
 
-from utils import flatten
+from utils import flatten, flatten_dicts
 
 
 class NaiveFloquetifiedColourCode(Code):
     def __init__(self, tiles_width: int, tiles_height: int):
+        if tiles_width <= 0 or tiles_width % 3 == 0:
+            raise ValueError(
+                "Width in terms of tiles must be a positive multiple of 3. " +
+                f"Instead, got {tiles_width}")
+        if tiles_height <= 0:
+            raise ValueError(
+                "Height in terms of tiles must be a positive number. " +
+                f"Instead, got {tiles_height}")
         # Code is made out of tiles.
         # Each tile can be seen as two half-tiles vertically stacked,
         # with X and Z swapped between the two half tiles.
@@ -31,12 +40,17 @@ class NaiveFloquetifiedColourCode(Code):
         self.wonky_x_max, self.wonky_y_max = wonky_top_right[0], wonky_top_right[1]
 
         # Set the data qubits now so we can refer to them in a sec.
-        super().__init__(data_qubits=self.get_data_qubits())
+        super().__init__(data_qubits=self._get_data_qubits())
 
-        # Figure out the check schedule (fiddly!)
-        self.set_schedules(
-            self.get_check_schedule(),
-            self.get_detector_schedule())
+        # Figure out the check and detector schedules (fiddly!)
+        check_schedule = self._get_check_schedule()
+        detector_schedule = self._get_detector_schedule(check_schedule)
+
+        # Now convert check_schedule to a list of lists rather than list of dicts.
+        check_schedule = [
+            list(checks_dict.values())
+            for checks_dict in check_schedule]
+        self.set_schedules(check_schedule, detector_schedule)
 
     @staticmethod
     def to_wonky_coords(straight_coords: Tuple[int, int]):
@@ -66,7 +80,7 @@ class NaiveFloquetifiedColourCode(Code):
         # assuming the bottommost one is (0,0)
         return [(0, 0)] + [(x, y) for x in range(0, 6, 2) for y in range(2, 10, 2)]
 
-    def get_data_qubits(self):
+    def _get_data_qubits(self):
         # Figure out the (straight) coordinates of the data qubits.
         # Take the 13 qubit coords in each half tile and shift them around.
         data_qubit_coordss = [
@@ -79,11 +93,13 @@ class NaiveFloquetifiedColourCode(Code):
         data_qubits = {coords: Qubit(coords) for coords in data_qubit_coordss}
         return data_qubits
 
-    def get_check_schedule(self):
+    def _get_check_schedule(self):
+        # Initially, let check_schedule be a list of dicts.
+        # Keys for each dict will be the anchors of the corresponding checks.
         check_schedule = []
         # Get all the checks that occur in round 0,
         # one half-tile at a time.
-        check_schedule.append(flatten([
+        check_schedule.append(flatten_dicts([
             self._half_tile_checks_round_0((half_tile_x, half_tile_y))
             for half_tile_x in range(self._tiles_width)
             for half_tile_y in range(2 * self._tiles_height)]))
@@ -91,9 +107,9 @@ class NaiveFloquetifiedColourCode(Code):
         # The code has period 13 altogether.
         shift = np.array([-2, -6])
         for t in range(1, 13):
-            shifted_checks = []
-            for check in check_schedule[t-1]:
-                # Shift the anchor (anchor only relevant for printing the code).
+            shifted_checks = {}
+            for check in check_schedule[t-1].values():
+                # Shift the anchor.
                 shifted_anchor = self.wrap_straight_coords(tuple(shift + check.anchor))
                 # Then shift all of the Paulis.
                 shifted_paulis = {}
@@ -105,11 +121,11 @@ class NaiveFloquetifiedColourCode(Code):
                     pauli = Pauli(self.data_qubits[shifted_coords], pauli.letter)
                     shifted_paulis[key] = pauli
                 shifted_check = Check(shifted_paulis, shifted_anchor)
-                shifted_checks.append(shifted_check)
+                shifted_checks[shifted_anchor] = shifted_check
             check_schedule.append(shifted_checks)
         return check_schedule
 
-    def _half_tile_checks_round_0(self, half_tile_coords: Tuple[int, int]) -> List[Check]:
+    def _half_tile_checks_round_0(self, half_tile_coords: Tuple[int, int]) -> Dict[Tuple[int, int], Check]:
         # Get all checks in round 0 for the half-tile with the given coords.
         half_tile_x, half_tile_y = half_tile_coords
         even_half_tile_row = half_tile_y % 2 == 0
@@ -130,13 +146,15 @@ class NaiveFloquetifiedColourCode(Code):
             half_tile_x * self._half_tile_bottom_vector + \
             half_tile_y * self._half_tile_side_vector
 
-        checks = []
+        checks_dict = {}
         for raw_check in raw_checks:
             coordss, pauli_letter = raw_check
             # Set the check's anchor to be the midpoint of the two data qubits
             # (or just the data qubit itself, for the single qubit measurement).
             # (Anchor only relevant for nicely printing the toric geometry.
-            # Has no effect on what's actually measured in the code.)
+            # Has no effect on what's actually measured in the code.
+            # We also use it temporarily as a key for the check,
+            # for use in determining the detector schedule shortly.)
             anchor = tuple(np.mean(coordss, axis=0).astype(int) + shift)
             paulis = {}
             for coords in coordss:
@@ -147,16 +165,55 @@ class NaiveFloquetifiedColourCode(Code):
                 # Again, just relevant for printing what's happening.
                 from_anchor = tuple(np.array(coords) + shift - anchor)
                 paulis[from_anchor] = pauli
-            checks.append(Check(paulis, anchor))
-        return checks
+            wrapped_anchor = self.wrap_straight_coords(anchor)
+            checks_dict[wrapped_anchor] = Check(paulis, wrapped_anchor)
+        return checks_dict
 
-    def get_detector_schedule(self):
-        # Code has period 13
-        detector_schedule = []
-        for t in range(13):
-            # TODO!
-            detector_schedule.append([])
+    def _get_detector_schedule(self, check_schedule: List[Dict[Tuple[int, int], Check]]) -> List[List[Detector]]:
+        # Code has period 13.
+        # There is one detector per half-tile per round.
+        detector_schedule = [[
+                self._half_tile_detector((x, y), t, check_schedule)
+                for x in range(self._tiles_width)
+                for y in range(2 * self._tiles_height)]
+            for t in range(13)]
         return detector_schedule
+
+    def _half_tile_detector(
+            self,
+            half_tile_coords: Tuple[int, int],
+            round: int,
+            check_schedule: List[Dict[Tuple[int, int], Check]]):
+        half_tile_x, half_tile_y = half_tile_coords
+        # As with the check schedule, start with a sort of 'raw detector',
+        # containing the minimum information required to create a detector.
+        # In this case this is just the anchors of the checks involved
+        # and the timesteps at which they occur.
+        raw_detector = [
+            [(-3, 0), (0, -1), (2, -1)],
+            [(-3, -2), (1, 2)],
+            [(3, -2), (4, 2)],
+            [(4, -1)],
+            [(-4, 1)],
+            [(-4, -2), (-3, 2)],
+            [(-1, -2), (3, 2)],
+            [(-2, 1), (0, 1), (3, 0)]]
+        detector_anchor = (0, 6)
+        # One shift accounts for which half-tile we're in
+        shift_a = \
+            half_tile_x * self._half_tile_bottom_vector + \
+            half_tile_y * self._half_tile_side_vector
+        # Another shift accounts for the round we're in
+        shift_b = round * np.array([-2, -6])
+        shift = shift_a + shift_b
+        shifted_anchor = self.wrap_straight_coords(tuple(shift + detector_anchor))
+        detector_checks = [
+            (-t, check_schedule[round-t][self.wrap_straight_coords(tuple(np.array(shifted_anchor) + check_anchor))])
+            for t, check_anchors in enumerate(raw_detector)
+            for check_anchor in check_anchors]
+        detector = Detector(detector_checks, round, shifted_anchor)
+        return detector
+
 
 
 code = NaiveFloquetifiedColourCode(6, 2)
